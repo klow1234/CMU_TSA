e45a3f36d4b85b31236e9a7e6f3858fbd3dbbd2e
Yihuan Zhou <huan@oneblueham.com>
2012-05-23 00:02:01 -0700

    whoops forgot to commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CMUTsa::Application.config.secret_token = '3645e48d0cc0685f8a7ce42059ca3bbe50bec55e856ffdd5a6045d9befe84cf7f3eeef2f91cb6195dfb38f6e860c3d1bd495f9f9c48f5c0cfaa5ba5fa4128720'
