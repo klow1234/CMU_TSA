d0abeb8021c65001cf23c83e5fdc97df5f222a1c
Yihuan Zhou <huan@oneblueham.com>
2012-05-23 01:34:19 -0700

    done with home, about, contact

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120521233756) do
+ActiveRecord::Schema.define(:version => 20120523081854) do
 
   create_table "refinery_blog_categories", :force => true do |t|
     t.string   "title"
@@ -76,6 +76,18 @@ ActiveRecord::Schema.define(:version => 20120521233756) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
+
+  create_table "refinery_inquiries_inquiries", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "message"
+    t.boolean  "spam",       :default => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+  end
+
+  add_index "refinery_inquiries_inquiries", ["id"], :name => "index_refinery_inquiries_inquiries_on_id"
 
   create_table "refinery_page_part_translations", :force => true do |t|
     t.integer  "refinery_page_part_id"
