4a65abf64b7726bb1bf7e00b0f1f3a4c7975c550
Yihuan Zhou <huan@oneblueham.com>
2012-05-24 00:25:10 -0700

    styling event index view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
+
+  private
+
+  def upcoming_events
+    @upcoming_events #= Event.where('date >= ?', Time.now).order('date DESC').limit(5)
+  end
+  helper_method :upcoming_events
 end
