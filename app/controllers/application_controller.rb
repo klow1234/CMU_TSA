f5f4f7696a70bb6fc6c7ec38e0eed367c03a3c05
Yihuan Zhou <huan@oneblueham.com>
2012-05-24 03:09:12 -0700

    added upcoming events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   private
 
   def upcoming_events
-    @upcoming_events #= Event.where('date >= ?', Time.now).order('date DESC').limit(5)
+    @upcoming_events = Refinery::Events::Event.where('date >= ?', Time.now).order('date DESC').limit(5)
+    @upcoming_events
   end
   helper_method :upcoming_events
 end
