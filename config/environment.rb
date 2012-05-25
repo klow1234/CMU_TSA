8daf923bb2924a456d83f70df97bce152540319f
Yihuan Zhou <huan@oneblueham.com>
2012-05-25 01:05:48 -0700

    updated environment.rb to be able to send mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 CMUTsa::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address        => 'smtp.sendgrid.net',
+    :port           => '25',
+    :authentication => :plain,
+    :user_name      => 'myemail@domain.com',
+    :password       => 'mypassword',
+    :domain         => 'mydomain.com'
+}
