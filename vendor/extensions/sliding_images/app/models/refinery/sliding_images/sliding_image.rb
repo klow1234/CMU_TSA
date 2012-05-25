3c09d844069ace4573c2a8c353484b42790900cb
Yihuan Zhou <huan@oneblueham.com>
2012-05-25 13:06:40 -0700

    styling

--- vendor/extensions/sliding_images/app/models/refinery/sliding_images/sliding_image.rb
+++ vendor/extensions/sliding_images/app/models/refinery/sliding_images/sliding_image.rb
@@ -1,7 +1,7 @@
 module Refinery
   module SlidingImages
     class SlidingImage < Refinery::Core::BaseModel
-    	attr_accessible :name, :caption, :active, :photo_id
+    	attr_accessible :name, :caption, :active, :photo_id, :position
       self.table_name = 'refinery_sliding_images'      
     
       acts_as_indexed :fields => [:name, :caption]
